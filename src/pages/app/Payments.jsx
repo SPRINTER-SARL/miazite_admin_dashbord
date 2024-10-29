bd8f17ded721ee887ffcd835d91a757b0fef68d9
yalnguembu <yalnguembu@gmail.com>
2024-10-29 21:32:19 +0100

    feat:display client name in payment list

--- src/pages/app/Payments.jsx
+++ src/pages/app/Payments.jsx
@@ -77,6 +77,66 @@ const UsersList = () => {
     }
   };
 
+  const PaymentItem = ({ payment }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [client, setClient] = useState(false);
+
+    const fetchProfile = async () => {
+      setIsLoading(true);
+      try {
+        const response = payment.userId
+          ? await getDataByCondition(
+              "Users",
+              "uid",
+              "==",
+              payment?.userId ?? ""
+            )
+          : [];
+        console.log(response[0], payment.userId);
+
+        if (response.length) setClient(response[0]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      fetchProfile();
+    }, []);
+    return (
+      <div className="rounded border hover:bg-gray-100 cursor-pointer p-3">
+        <p className="text-base">
+          {Math.round(payment?.price * 96.05)} {" DZ"}
+        </p>
+        <div className="flex text-gray-500 items-center gap-3 py-2 divide-x">
+          <div className="flex gap-2 items-center text-sm">
+            <Icon icon="heroicons:calendar" size={20} />
+            <span className="font-bold">
+              {toLocaleDateString(payment.timestamp)}
+            </span>
+          </div>
+          <span
+            className={`ml-3 px-2 py-0.5 text-sm rounded-full ${
+              payment?.status === "Payé"
+                ? "bg-green-200 text-green-600"
+                : payment?.status === "Impayé"
+                ? "bg-red-200 text-red-400"
+                : "bg-orange-200 text-orange-500"
+            }`}
+          >
+            {payment.status}
+          </span>
+          <span className="pl-3">{payment.type}</span>
+        </div>
+        <p className="text-sm text-gray-500 mb-2">
+          {isLoading ? "Loading..." : client?.nom + " " + client?.prenom}
+        </p>
+      </div>
+    );
+  };
+
   return (
     <div>
       <div className="w-full border-b border-b-gray-100">
@@ -123,35 +183,11 @@ const UsersList = () => {
                 <div className="w-full border-t justify-end pt-4">
                   <div className="grid grid-cols-2 gap-3 mt-3">
                     {payments.map((payment) => (
-                      <div
+                      <PaymentItem
                         key={payment?.id}
                         onClick={() => displayPaymentProof(payment)}
-                        className="rounded border hover:bg-gray-100 cursor-pointer p-3"
-                      >
-                        <p className="text-base">
-                          {Math.round(payment?.price * 96.05)} {" DZ"}
-                        </p>
-                        <div className="flex text-gray-500 items-center gap-3 py-2 divide-x">
-                          <div className="flex gap-2 items-center text-sm">
-                            <Icon icon="heroicons:calendar" size={20} />
-                            <span className="font-bold">
-                              {toLocaleDateString(payment.timestamp)}
-                            </span>
-                          </div>
-                          <span
-                            className={`ml-3 px-2 py-0.5 text-sm rounded-full ${
-                              payment?.status === "Payé"
-                                ? "bg-green-200 text-green-600"
-                                : payment?.status === "Impayé"
-                                ? "bg-red-200 text-red-400"
-                                : "bg-orange-200 text-orange-500"
-                            }`}
-                          >
-                            {payment.status}
-                          </span>
-                          <span className="pl-3">{payment.type}</span>
-                        </div>
-                      </div>
+                        payment={payment}
+                      />
                     ))}
                   </div>
                 </div>
